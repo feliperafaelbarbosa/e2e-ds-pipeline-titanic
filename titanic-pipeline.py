461cc61b50772ebad3d6362a667f66f0f004b51c
Felipe Rafael Barbosa <rafaelt.ibarbosa@gmail.com>
2024-11-26 02:44:26 -0300

    Machine Learning and Visualization

--- titanic-pipeline.py
+++ titanic-pipeline.py
@@ -64,3 +64,32 @@ preprocessor = ColumnTransformer(
 # %%
 X_train = preprocessor.fit_transform(X_train)
 X_test = preprocessor.transform(X_test)
+
+# %% Machine Learning
+from sklearn.linear_model import LogisticRegression
+import joblib
+
+# %%
+model = LogisticRegression(random_state = 42)
+model.fit(X_train, y_train)
+
+# %%
+joblib.dump(model, 'titanic_logistic_regression_model.joblib')
+joblib.dump(preprocessor, 'titanic_preprocessor.joblib')
+
+# %%
+accuracy = model.score(X_test, y_test)
+print(accuracy)
+
+# %% Visualization
+import matplotlib.pyplot as plt
+import numpy as np
+
+importance = model.coef_[0]
+features = np.array(numerical_feature + list(preprocessor.named_transformers_['cat']['onehot'].get_feature_names_out(categorical_feature)))
+
+plt.figure(figsize=(10, 8))
+plt.barh(features, importance)
+plt.xlabel('Importance')
+plt.title('Feature Importance')
+plt.show()
